fff30dd1279036e0f76e949372d44d10b9bd0f7c
aniruddha <aniruddha.kudalkar@gmail.com>
2017-05-08 11:22:22 +0530

    ui code : event listeners and layout params

--- app/src/main/java/com/codekul/app/MainActivity.java
+++ app/src/main/java/com/codekul/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.codekul.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -14,18 +15,25 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout layoutRoot = new LinearLayout(this);
         layoutRoot.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams paramsRoot = new LinearLayout.LayoutParams(500,500);
+        LinearLayout.LayoutParams paramsRoot = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
         layoutRoot.setLayoutParams(paramsRoot);
 
-        EditText edtName = new EditText(this);
+        final EditText edtName = new EditText(this);
         edtName.setHint("Any Name");
-        LinearLayout.LayoutParams paramsName = new LinearLayout.LayoutParams(500, 150);
+        LinearLayout.LayoutParams paramsName = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         edtName.setLayoutParams(paramsName);
         layoutRoot.addView(edtName);
 
         Button btnOkay = new Button(this);
+        btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                edtName.setText(name.toUpperCase());
+            }
+        });
         btnOkay.setText("Okay");
-        LinearLayout.LayoutParams paramsBtn = new LinearLayout.LayoutParams(500, 100);
+        LinearLayout.LayoutParams paramsBtn = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         btnOkay.setLayoutParams(paramsBtn);
         layoutRoot.addView(btnOkay);
 
